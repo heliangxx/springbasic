f89d57db333ddf98d119ff221759deaf2ccbbfce
heliangxx <heliangxx@163.com>
2019-10-10 15:46:21 +0800

    mybatisplus add

--- src/main/java/com/pactera/springbasic/StartApplication.java
+++ src/main/java/com/pactera/springbasic/StartApplication.java
@@ -2,6 +2,7 @@ package com.pactera.springbasic;
 
 import com.pactera.common.database.DynamicDataSourceRegister;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.pactera" })
 @Import({ DynamicDataSourceRegister.class })
+@MapperScan("com.pactera.springbasic.mapper")
 public class StartApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/pactera/springbasic/mapper/SysMenuMapper.java
@@ -0,0 +1,8 @@
+package com.pactera.springbasic.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pactera.springbasic.Entity.SysMenu;
+
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+}
